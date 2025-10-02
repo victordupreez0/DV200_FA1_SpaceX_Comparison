2d69c092ccbefe5e4a108135e7191aed898147d5
Victor DP <victor.dupreez0@gmail.com>
2025-10-02 11:31:51 +0200

    Fix Heroku deploy

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
+// Catch-all handler: send back React's index.html for any request not handled
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
